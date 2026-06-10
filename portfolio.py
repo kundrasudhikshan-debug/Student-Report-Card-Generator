1ccae07960638f651e8213be528caae76a99579d
Itachi_Uchiha <kundrasudhikshan@gmail.com>
2026-06-10 14:38:38 +0530

    Added skills section

--- portfolio.py
+++ portfolio.py
@@ -3,6 +3,7 @@ print("=== Personal Portfolio ===")
 name = "Sudhikshan"
 age = 16
 hobbies = ["Programming", "Mathematics", "Minecraft"]
+skills = ["Python", "Git", "Problem Solving"]
 
 print("\nName:", name)
 print("Age:", age)
@@ -10,5 +11,9 @@ print("Age:", age)
 print("\nHobbies:")
 for hobby in hobbies:
     print("-", hobby)
+
+print("\nSkills:")
+for skill in skills:
+    print("-", skill)
 
 print("\nThank you for visiting my portfolio!")
