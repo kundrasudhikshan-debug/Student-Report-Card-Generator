2466a30b4a093f7257d07930e1fa436493e06cdd
HK <h@k>
2026-06-10 23:38:39 +0530

    by papa

--- portfolio.py
+++ portfolio.py
@@ -8,6 +8,8 @@ skills = ["Python", "Git", "Problem Solving"]
 print("\nName:", name)
 print("Age:", age)
 
+this is some bad code that i am writing to show git to some kiddo
+
 print("\nHobbies:")
 for hobby in hobbies:
     print("-", hobby)
