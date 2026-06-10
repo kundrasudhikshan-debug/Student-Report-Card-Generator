4bbcc6e582773c23c45b65c13f4cb5bc55be1e70
Itachi_Uchiha <kundrasudhikshan@gmail.com>
2026-06-10 14:38:23 +0530

    Created basic portfolio

---
+++ portfolio.py
@@ -0,0 +1,14 @@
+print("=== Personal Portfolio ===")
+
+name = "Sudhikshan"
+age = 16
+hobbies = ["Programming", "Mathematics", "Minecraft"]
+
+print("\nName:", name)
+print("Age:", age)
+
+print("\nHobbies:")
+for hobby in hobbies:
+    print("-", hobby)
+
+print("\nThank you for visiting my portfolio!")
